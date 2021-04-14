6f0b022a9567f4f4f3c0e765f28dc8ca58262524
starkgines <csilvagines@gmail.com>
2021-04-14 11:08:05 -0400

    se agrego los cuadernos de la clase 7

--- Clases/Clase 2/App.py
+++ Clases/Clase 2/App.py
@@ -9,16 +9,16 @@ a.setNombre("Juan")
 
 a.saludar()
 
-
+# 1. El nombre del atributo , 2. El valor que deseo dar al atributo
 a.__setattr__("Nombre","Rodrigo")
 
 a.saludar()
-
+# 1. El nombre del atributo que quiero que retorne 
 print(a.__getattribute__("Nombre"))
 
-
+# Retorna los atributos de mi objeto con sus respectivos valores
 print(a.__dict__)
-
+# Los nombres de todos los atributos del obejeto
 atributos = a.__dict__.keys()
 
 print(atributos)
@@ -26,11 +26,12 @@ print(atributos)
 
 for x in atributos:
     print(x)
-
     aux = input("ingrese un valor para el {}: ".format(x))
     a.__setattr__(x,aux)
 
 
 a.saludar()
+
+
 
 print("Exito")
